28da1d09031cddb45c41d03e833d2aefc804a5d6
averyanalex <averyanalex@gmail.com>
2021-04-06 15:32:33 +0300

    Add trowns exception

--- LauncherAPI/src/main/java/pro/gravit/launcher/request/websockets/StdWebSocketService.java
+++ LauncherAPI/src/main/java/pro/gravit/launcher/request/websockets/StdWebSocketService.java
@@ -29,7 +29,7 @@ public class StdWebSocketService extends ClientWebSocketService {
         super(address);
     }
 
-    public static StdWebSocketService initWebSockets(String address, boolean async) {
+    public static StdWebSocketService initWebSockets(String address, boolean async) throws IOException, URISyntaxException{
         StdWebSocketService service;
         try {
             service = new StdWebSocketService(address);
